95d6c8832ba7cc64fbd84a9b08bd236cbfacbe74
Quoc Khanh Nguyen <khanh.nguyen@arrabiata.de>
2016-01-08 18:35:39 +0100

    Add old blog example

---
+++ typo3conf/ext/blog_example/ext_emconf.php
@@ -0,0 +1,42 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "blog_example".
+ *
+ * Auto generated 08-01-2016 10:47
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'A Blog Example for the Extbase Framework',
+	'description' => 'An example extension demonstrating the features of the Extbase Framework. It is the back-ported and tweaked Blog Example package of FLOW3. Have fun playing with it!',
+	'category' => 'example',
+	'version' => '1.3.0',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearcacheonload' => 1,
+	'author' => 'TYPO3 core team',
+	'author_email' => '',
+	'author_company' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
+			'extbase' => '1.3.0-0.0.0',
+			'fluid' => '1.3.0-0.0.0',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
