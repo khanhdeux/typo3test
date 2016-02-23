79aa8ae7e6d9471765ead634845ac54624d4a639
Khanhdeux Mac <khanhdeux@gmail.com>
2016-02-23 23:41:16 +0100

    Add EU countries on select fluid

---
+++ typo3conf/ext/static_info_tables/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables".
+ *
+ * Auto generated 23-02-2016 22:43
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Static Info Tables',
+	'description' => 'Data and API for countries, languages and currencies.',
+	'category' => 'misc',
+	'version' => '6.3.6',
+	'state' => 'stable',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'author' => 'Stanislas Rolland/René Fritz',
+	'author_email' => 'typo3@sjbr.ca',
+	'author_company' => 'SJBR',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.0-7.6.99',
+			'php' => '5.3.7-0.0.0',
+		),
+		'conflicts' => 
+		array (
+			'sr_static_info' => '0.0.0-99.99.99',
+			'cc_infotablesmgm' => '0.0.0-99.99.99',
+			'uncache' => '0.0.0-99.99.99',
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'clearcacheonload' => false,
+);
+
